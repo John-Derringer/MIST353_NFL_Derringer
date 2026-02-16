2192086674c054815e7acbcf2dc21e0c6f4fe261
John-Derringer <jmd00043@mix.wvu.edu>
2026-02-16 13:20:30 -0500

    Inserted all division data and inserted all AFC North teams into their Conference/Division..

--- Data/InsertDataLastname.sql
+++ Data/InsertDataLastname.sql
@@ -1,3 +1,21 @@
 -- Insert data
 -- Insert all the ConferenceDivision data (8 rows)
 -- Insert team data for AFC North (4 rows)
+
+INSERT INTO ConferenceDivision (ConferenceDivisionID, Conference, Division)
+    VALUES
+    (1, 'AFC', 'North'),
+    (2, 'AFC', 'South'),
+    (3, 'AFC', 'East'),
+    (4, 'AFC', 'West'),
+    (5, 'NFC', 'North'),
+    (6, 'NFC', 'South'),
+    (7, 'NFC', 'East'),   
+    (8, 'NFC', 'West');
+
+INSERT INTO Team (TeamID, TeamName, TeamCityState, TeamColors, ConferenceDivisionID)
+    VALUES
+    (1, 'Pittsburgh Steelers', 'Pittsburgh,PA', 'Black,Yellow',1),
+    (2, 'Baltimore Ravens', 'Baltimore, MD', 'Black,Purple', 1),
+    (3, 'Cincinnati Bengals', 'Cincinnati, OH', 'Black,Orange', 1),
+    (4, 'Cleveland Browns', 'Cleveland, OH', 'Brown,Orange', 1);
